a63d3723b9caf5d5dcc58bc2c3454099a4086a6c
Sam Clark <sam@samclark.net>
2021-01-17 13:15:03 -0500

    Delete synch.sh

--- plot/synch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-# run with ./synch.sh
-# make sure you've run chmod 700 synch.sh
-
-# synch plotting project
-rsync -av --delete "/Users/samueljclark/Desktop/PNAS Commentary on Indiana Prevalence Method Paper/cdc-cv19-data/" /Users/samueljclark/Documents/GitHub/usaDeaths2020/plot
-
